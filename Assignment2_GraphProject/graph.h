1bcafe5b7d4af429e2c0d42288fcb24850f454b3
Your Name <your-email@example.com>
2026-05-26 17:19:32 +1000

    Added adjacency list graph structure

--- Assignment2_GraphProject/graph.h
+++ Assignment2_GraphProject/graph.h
@@ -2,4 +2,26 @@
 #ifndef GRAPH_H
 #define GRAPH_H
 
+#define MAX_LOCATIONS 20
+#define NAME_SIZE 50
+
+typedef struct edge {
+    int to;
+    int time;
+    struct edge* next;
+} Edge;
+
+typedef struct graph {
+    int numLocations;
+    char names[MAX_LOCATIONS][NAME_SIZE];
+    Edge* list[MAX_LOCATIONS];
+} Graph;
+
+void initGraph(Graph* g);
+void addLocation(Graph* g, int index, char name[]);
+void addRoad(Graph* g, int from, int to, int time);
+void printGraph(Graph* g);
+void freeGraph(Graph* g);
+void testGraph();
+
 #endif
