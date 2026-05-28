91cfac43e5666c0e11ca455fcb730d3f3f928907
Your Name <your-email@example.com>
2026-05-28 23:19:41 +1000

    Implemented basic Dijkstra shortest path algorithm

--- Assignment2_GraphProject/dijkstra.c
+++ Assignment2_GraphProject/dijkstra.c
@@ -1,2 +1,67 @@
 #include <stdio.h>
 #include "dijkstra.h"
+
+int findSmallestDistance(int distance[], int visited[], int numLocations) {
+    int i;
+    int smallest;
+    int smallestIndex;
+
+    smallest = INF;
+    smallestIndex = -1;
+
+    for (i = 0; i < numLocations; i++) {
+        if (visited[i] == 0 && distance[i] < smallest) {
+            smallest = distance[i];
+            smallestIndex = i;
+        }
+    }
+
+    return smallestIndex;
+}
+
+void dijkstra(Graph* g, int start, int end) {
+    int distance[MAX_LOCATIONS];
+    int visited[MAX_LOCATIONS];
+    int i;
+    int count;
+    int currentIndex;
+    int newDistance;
+    Edge* currentEdge;
+
+    for (i = 0; i < g->numLocations; i++) {
+        distance[i] = INF;
+        visited[i] = 0;
+    }
+
+    distance[start] = 0;
+
+    for (count = 0; count < g->numLocations - 1; count++) {
+        currentIndex = findSmallestDistance(distance, visited, g->numLocations);
+
+        if (currentIndex == -1) {
+            break;
+        }
+
+        visited[currentIndex] = 1;
+        currentEdge = g->list[currentIndex];
+
+        while (currentEdge != NULL) {
+            newDistance = distance[currentIndex] + currentEdge->time;
+
+            if (visited[currentEdge->to] == 0 && newDistance < distance[currentEdge->to]) {
+                distance[currentEdge->to] = newDistance;
+            }
+
+            currentEdge = currentEdge->next;
+        }
+    }
+
+    printf("Shortest travel time from %s to %s: ", g->names[start], g->names[end]);
+
+    if (distance[end] == INF) {
+        printf("No route found.\n");
+    }
+    else {
+        printf("%d minutes\n", distance[end]);
+    }
+}
--- Assignment2_GraphProject/dijkstra.h
+++ Assignment2_GraphProject/dijkstra.h
@@ -1,7 +1,10 @@
-#pragma once
 #ifndef DIJKSTRA_H
 #define DIJKSTRA_H
 
 #include "graph.h"
+
+#define INF 999999
+
+void dijkstra(Graph* g, int start, int end);
 
 #endif
