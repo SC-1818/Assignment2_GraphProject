bf649b00c4463d2b42b5b07cd61181493c154713
Your Name <your-email@example.com>
2026-05-31 11:35:37 +1000

    Connected Dijkstra test to main program

--- Assignment2_GraphProject/main.c
+++ Assignment2_GraphProject/main.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 #include "graph.h"
+#include "dijkstra.h"
 
 int main() {
     printf("Emergency Route Planning in a Road Network\n");
-    printf("Part 1: Graph Structure Test\n\n");
+    printf("Part 2: Dijkstra Shortest Path Test\n\n");
 
     testGraph();
+    testDijkstra();
 
     return 0;
 }
